d0c16545d564b582f09a11d12be1168822915230
Muhd Saifulnizam <muhdsaifulnizam1998@gmail.com>
2020-12-19 10:57:17 +0800

    feat: 🎸 Added forceJsonResponse middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\App\Http\Middleware\ForceJsonResponse::class,
         \App\Http\App\Http\Middleware\Cors::class
     ];
 
@@ -62,7 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
+        'json.response' => \App\Http\Middleware\ForceJsonResponse::class,
+        'cors' => \App\Http\App\Http\Middleware\Cors::class
+    ];  
 
     /**
      * The priority-sorted list of middleware.
